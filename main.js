0fcf82954e2add4248407db395f47fc58fd1db90
MacBench <81919768+Helder-Machava@users.noreply.github.com>
2023-09-25 17:00:05 +0200

    Base Source Code forum

---
+++ main.js
@@ -0,0 +1,141 @@
+//Define quanto tempo é que a função LoadData deve ser executada a cada milisecond
+var myVar = setInterval(LoadData, 1000);
+
+//
+// http_request = new XMLHttpRequest();
+
+function LoadData() {
+	$.ajax({
+		url: 'view.php',
+		type: "POST",
+		dataType: 'json',
+		success: function (data) {
+
+			//	Limpa todos os objectos HTML filhos do tbody para mostrar a info actualizada puxada pela chamada AJAX
+			$('#MyTable tbody').empty();
+
+			//Percorre os dados fornecidos pela reposta via AJAX
+			for (var i = 0; i < data.length; i++) {
+
+				var commentId = data[i].id;
+
+				//Mostrar os Post Principais
+				if (data[i].parent_comment == 0) {
+
+					var row = $('<tr><td><img src="avatar.jpg" width="30px" height="30px" /> <b>' + data[i].student + ' - ' + data[i].date + '</b></br><p style="padding-left:80px">' + data[i].post + '</br><a data-toggle="modal" data-id="' + commentId + '" title="Add this item" class="open-ReplyModal" href="#ReplyModal">Reply</a>' + '</p></td></tr>');
+
+					// Encapsule| Adicione ao elemento tbody os objectos da variavel row como filhos da tbody 
+					$('#record').append(row);
+
+
+					//Mostrar as respostas dos Posts
+					for (var r = 0; r < data.length; r++) {
+						if (data[r].parent_comment == commentId) {
+							var comments = $('<tr><td style="padding-left:80px"><b><img src="avatar.jpg" width="30px" height="30px" />' + data[r].student + ' :<i> ' + data[r].date + '</i></b></br><p style="padding-left:40px">' + data[r].post + '</p></td></tr>');
+						
+							// Encapsule| Adicione ao elemento tbody os objectos da variavel row como filhos da tbody 
+							$('#record').append(comments);
+						}
+					}
+				}
+			}
+		},
+		error: function (jqXHR, textStatus, errorThrown) {
+			alert('Error: ' + textStatus + ' - ' + errorThrown);
+		}
+	});
+}
+
+
+// Ao Clicar no link *Reply*, Puxe o valor do atributo => "data-id"
+// Jogue este valor para o <input> oculto que está no Modal que vai abrir
+
+$(document).on("click", ".open-ReplyModal", function () {
+	var commentid = $(this).data('id');
+	$(".modal-body #commentid").val(commentid);
+});
+
+
+
+//Post data to the server
+$(document).ready(function () {
+	$('#butsave').on('click', function () {
+		$("#butsave").attr("disabled", "disabled");
+		var id = document.forms["frm"]["Pcommentid"].value;
+		var name = document.forms["frm"]["name"].value;
+		var msg = document.forms["frm"]["msg"].value;
+		if (name != "" && msg != "") {
+			$.ajax({
+				url: "save.php",
+				type: "POST",
+				data: {
+					id: id,
+					name: name,
+					msg: msg,
+				},
+				cache: false,
+				success: function (dataResult) {
+					var dataResult = JSON.parse(dataResult);
+					if (dataResult.statusCode == 200) {
+						$("#butsave").removeAttr("disabled");
+						document.forms["frm"]["Pcommentid"].value = "";
+						document.forms["frm"]["name"].value = "";
+						document.forms["frm"]["msg"].value = "";
+						LoadData();
+					}
+					else if (dataResult.statusCode == 201) {
+						alert("Error occured !");
+					}
+
+				}
+			});
+		}
+		else {
+			alert('Please fill all the field !');
+		}
+	});
+});
+
+//Reply comment
+$(document).ready(function () {
+	$('#btnreply').on('click', function () {
+		$("#btnreply").attr("disabled", "disabled");
+		var id = document.forms["frm1"]["Rcommentid"].value;
+		var name = document.forms["frm1"]["Rname"].value;
+		var msg = document.forms["frm1"]["Rmsg"].value;
+		if (name != "" && msg != "") {
+			$.ajax({
+				url: "save.php",
+				type: "POST",
+				data: {
+					id: id,
+					name: name,
+					msg: msg,
+				},
+				cache: false,
+				success: function (dataResult) {
+					var dataResult = JSON.parse(dataResult);
+					if (dataResult.statusCode == 200) {
+						$("#btnreply").removeAttr("disabled");
+						document.forms["frm1"]["Rcommentid"].value = "";
+						document.forms["frm1"]["Rname"].value = "";
+						document.forms["frm1"]["Rmsg"].value = "";
+						LoadData();
+						$("#ReplyModal").modal("hide");
+					}
+					else if (dataResult.statusCode == 201) {
+						alert("Error occured !");
+					}
+
+				}
+			});
+		}
+		else {
+			alert('Please fill all the field !');
+		}
+	});
+});
+
+
+
+
